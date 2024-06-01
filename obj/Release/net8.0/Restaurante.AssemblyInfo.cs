2abcc796bb60dc52ea08496049783da7385190aa
Juan Visney <jvcode23@gmail.com>
2024-06-01 11:18:33 -0400

    feat: creacion de modelo

--- obj/Release/net8.0/Restaurante.AssemblyInfo.cs
+++ obj/Release/net8.0/Restaurante.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurante")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd00866ee26ac0e32ee77f5b3eb4581f25df29f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurante")]
