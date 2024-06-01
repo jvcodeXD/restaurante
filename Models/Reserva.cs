2b1ebd27b8cd1f80bf0c406f1261ae57027e7a3b
lili_lh <lilianherrera14.lh@gmail.com>
2024-06-01 11:21:39 -0400

    feat: agregado del modelo reserva

---
+++ Models/Reserva.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Restaurante.Models
+{
+    public class Reserva
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Key]
+        public int IdCliente { get; set; }
+        [Required]
+        public string? Estado { get; set; }
+        [Required]
+        public DateTime Fecha { get; set;}
+    }
+}
