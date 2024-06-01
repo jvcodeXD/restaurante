fa414234fb2a16901c66c7517dd852c34c7f671b
Jhimy Inca <jhimy.inca@sistemas.edu.bo>
2024-06-01 11:23:18 -0400

    feat: Modelos Detalle y Contexto

---
+++ Models/Detalle.cs
@@ -0,0 +1,17 @@
+﻿using Restaurante.Dto;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Restaurante.Models
+{
+    public class Detalle
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public int Pedido { get; set; }
+        [Required]
+        public int Producto { get; set; }
+    }
+}
