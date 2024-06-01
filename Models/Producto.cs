4228541ae1f91312aaee31659da176d5eb662734
Kenny Ventura <kvlshield@gmail.com>
2024-06-01 11:25:01 -0400

    feat: creacion del modelo

---
+++ Models/Producto.cs
@@ -0,0 +1,25 @@
+﻿using Restaurante.Dto;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Restaurante.Models
+{
+    public class Producto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Descripcion { get; set; }
+
+        [Required]
+        public int Cantidad { get; set; }
+
+        [Required]
+        public string? Nombre { get; set; }
+
+        [Required]
+        public double Precio { get; set; }
+    }
+}
